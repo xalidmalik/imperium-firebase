6dc8c493a87fe6ddf98d2f8bcf03886abcc90400
Aga Huseynov <agahuseynov@hotmail.com>
2019-11-22 06:20:41 +0000

    package json

---
+++ src/firebase/index.ts
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALJ4IvoM0ZQ7B3UlGrGf8BJxP44-SaA-w",
+  authDomain: "routeax-carax.firebaseapp.com",
+  databaseURL: "https://routeax-carax.firebaseio.com",
+  projectId: "routeax-carax",
+  storageBucket: "routeax-carax.appspot.com",
+  messagingSenderId: "95886086992",
+  appId: "1:95886086992:web:88da01797f49455e671255",
+  measurementId: "G-FZY4Z6X2M0"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const db: any = firebase.firestore();
+const auth: any = firebase.auth();
+
+export { db, auth };
