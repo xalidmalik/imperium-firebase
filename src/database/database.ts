a18dab72eddeb14b26757a0ab2e7029fec8ae7e0
Aga Huseynov <agahuseynov@hotmail.com>
2019-11-22 07:06:09 +0000

    databases

---
+++ src/database/database.ts
@@ -0,0 +1,5 @@
+import React from "react";
+import db from "../firebase/firebaseconfig";
+import { DocumentTypes } from "helpers/Database/DocumentTypes";
+
+const GetRecords = (type: DocumentTypes) => {};
