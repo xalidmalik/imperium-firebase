43b0d30c0e2949f0d712d119261596014c33a668
xezer.quluzade95@gmail.com <xezer.quluzade95@gmail.com>
2019-11-24 17:33:15 +0300

    caroverview

--- src/containers/Overviews/CarOverview.tsx
+++ src/containers/Overviews/CarOverview.tsx
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import Card from "../../components/Card/Card";
 import { car } from "../../helpers/Static/System";
 import { carForm } from "../../helpers/Static/FormFields";
-// import FieldOutput from "../../components/FormElements/Outputs/FieldOutput";
-// import CheckboxOutput from "../../components/FormElements/Outputs/CheckboxOutput";
+import FieldOutput from "../../components/FormElements/Output/FieldOutput";
+import CheckboxOutput from "../../components/FormElements/Output/CheckboxOutput";
 
 export class CarOverview extends Component {
   render() {
@@ -13,42 +13,32 @@ export class CarOverview extends Component {
       (
         <>
           <Card base={car.basicfeatures}>
-            {/* <FieldOutput base={carForm.BrandId} data={data.BrandName} />
-            <FieldOutput base={carForm.BrandModelId} data={data.ModelName} />
-            <FieldOutput
-              base={carForm.CaseType}
-              data={StringCaseTypeList(data.CaseType)}
-            />
+            <FieldOutput base={carForm.BrandName} data={data.BrandName} />
+            <FieldOutput base={carForm.ModelName} data={data.ModelName} />
+            <FieldOutput base={carForm.CaseType} data={data.CaseType} />
             <FieldOutput
               base={carForm.EngineCapacity}
               data={data.EngineCapacity}
             />
             <FieldOutput base={carForm.Year} data={data.Year} />
-            <FieldOutput
-              base={carForm.NumberOfDoors}
-              data={data.NumberOfDoors}
-            />
-            <FieldOutput
-              base={carForm.FuelType}
-              data={StringFuelType(data.FuelType)}
-            />
-            <FieldOutput
-              base={carForm.GearType}
-              data={StringGearType(data.GearType)}
-            />
-            <FieldOutput
-              base={carForm.Classes}
-              data={StringCarClasses(data.Classes)}
-            />
+            <FieldOutput base={carForm.DoorsNumber} data={data.DoorsNumber} />
+            <FieldOutput base={carForm.FuelType} data={data.FuelType} />
+            <FieldOutput base={carForm.GearType} data={data.GearType} />
+            <FieldOutput base={carForm.Classes} data={data.Classes} />
           </Card>
           <Card base={car.additionalinfo}>
             <FieldOutput base={carForm.Plate} data={data.Plate} />
+            <FieldOutput base={carForm.Deposit} data={data.Deposit} />
             <FieldOutput base={carForm.Price} data={data.Price} />
-            <FieldOutput base={carForm.Km} data={data.Km} />
+            <FieldOutput base={carForm.KM} data={data.KM} />
             <FieldOutput
-              base={carForm.MinDriverLicense}
-              data={data.MinDriverLicense}
-            /> */}
+              base={carForm.MinDriverLicenseYear}
+              data={data.MinDriverLicenseYear}
+            />
+            <FieldOutput
+              base={carForm.MinDrivingLicenseYear}
+              data={data.MinDrivingLicenseYear}
+            />
           </Card>
         </>
       )
