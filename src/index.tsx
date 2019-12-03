f64c0f0caa4f270f16fd352c6765bbc71971c974
Agha Huseynov <agahuseynov@hotmail.com>
2019-12-03 21:35:59 +0000

    false false

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Router as BrowserRouter } from "react-router-dom";
 import { History } from "./helpers/Static/History";
-require('dotenv').config()
+require("dotenv").config();
+
+process.env.CI = "false";
 
 ReactDOM.render(
   <BrowserRouter history={History}>
