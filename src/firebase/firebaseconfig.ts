6e9c1d74ad3a769ac1e460323e51e0861e224184
xalidmalik <halidmalik@irisakademi.com.tr>
2020-04-01 03:09:32 +0300

    change 2

--- src/firebase/firebaseconfig.ts
+++ src/firebase/firebaseconfig.ts
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyALJ4IvoM0ZQ7B3UlGrGf8BJxP44-SaA-w",
-  authDomain: "routeax-carax.firebaseapp.com",
-  databaseURL: "https://routeax-carax.firebaseio.com",
-  projectId: "routeax-carax",
-  storageBucket: "routeax-carax.appspot.com",
-  messagingSenderId: "95886086992",
-  appId: "1:95886086992:web:88da01797f49455e671255",
-  measurementId: "G-FZY4Z6X2M0"
+    authDomain: "routeax-carax.firebaseapp.com",
+    databaseURL: "https://routeax-carax.firebaseio.com",
+    projectId: "routeax-carax",
+    storageBucket: "routeax-carax.appspot.com",
+    messagingSenderId: "95886086992",
+    appId: "1:95886086992:web:90542f91b07bd4ea671255",
+    measurementId: "G-HKQM90RHEB"
 };
 
 firebase.initializeApp(firebaseConfig);
